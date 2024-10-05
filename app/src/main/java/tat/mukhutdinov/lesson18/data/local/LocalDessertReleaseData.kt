f3abbfc2bef1662ee7e89ec7086a4b5d61f8ea9c
Bulat <bulatmm@yandex.ru>
2024-10-05 19:03:03 +0500

    Initial commit

---
+++ app/src/main/java/tat/mukhutdinov/lesson18/data/local/LocalDessertReleaseData.kt
@@ -0,0 +1,28 @@
+package tat.mukhutdinov.lesson18.data.local
+
+/*
+ * Set data for Android Release dessert name
+ * the list contain distinct sequential dessert name
+ */
+object LocalDessertReleaseData {
+    val dessertReleases = listOf(
+        "Cupcake",
+        "Donut",
+        "Eclair",
+        "Froyo",
+        "Gingerbread",
+        "Honeycomb",
+        "Ice Cream Sandwich",
+        "Jelly Bean",
+        "KitKat",
+        "Lollipop",
+        "Marshmallow",
+        "Nougat",
+        "Oreo",
+        "Pie",
+        "Quince Tart",
+        "Red Velvet Cake",
+        "Snow Cone",
+        "Tiramisu"
+    )
+}
